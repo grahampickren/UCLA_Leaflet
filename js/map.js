bf039c26ba52e3f98591380affef0db0d009cdc5
grahampickren <pickrengw@gmail.com>
2025-03-07 10:37:45 -0500

    Test map

---
+++ js/map.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([34.0697,-85.4432], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+	
